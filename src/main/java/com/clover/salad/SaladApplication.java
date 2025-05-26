65344b0ed638ea29155cec8f309ba225ecf7069c
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-05-26 11:49:37 +0900

    feat : 초기 세팅

---
+++ src/main/java/com/clover/salad/SaladApplication.java
@@ -0,0 +1,13 @@
+package com.clover.salad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaladApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaladApplication.class, args);
+	}
+
+}

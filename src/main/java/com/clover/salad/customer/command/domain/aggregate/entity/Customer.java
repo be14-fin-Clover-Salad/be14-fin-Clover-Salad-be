7cb2a44cd5baa191e3dd52477efdf0e321f89ed9
50071 <rkd50071@gmail.com>
2025-05-29 18:23:16 +0900

    localdatetime를 localdate로 수정

--- src/main/java/com/clover/salad/customer/command/domain/aggregate/entity/Customer.java
+++ src/main/java/com/clover/salad/customer/command/domain/aggregate/entity/Customer.java
@@ -1,6 +1,6 @@
 package com.clover.salad.customer.command.domain.aggregate.entity;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -36,11 +36,14 @@ public class Customer {
 	@Column(name = "phone")
 	private String phone;
 
+	@Column(name = "address")
+	private String address;
+
 	@Column(name = "email")
 	private String email;
 
 	@Column(name = "register_at")
-	private LocalDateTime registerAt;
+	private LocalDate registerAt;
 
 	@Column(name = "is_deleted")
 	private boolean isDeleted;

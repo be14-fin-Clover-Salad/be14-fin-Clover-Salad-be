8c790d594092a5a01edc7980fe82a8f07c5e1250
50071 <rkd50071@gmail.com>
2025-06-03 12:35:18 +0900

    Refactor: DTO property 수정

--- src/main/java/com/clover/salad/customer/query/dto/CustomerQueryDTO.java
+++ src/main/java/com/clover/salad/customer/query/dto/CustomerQueryDTO.java
@@ -1,5 +1,7 @@
 package com.clover.salad.customer.query.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,7 +16,9 @@ public class CustomerQueryDTO {
     private String phone;
     private String address;
     private String email;
+    @JsonProperty("registerAt")
     private String registerAt;
+    @JsonProperty("isDeleted")
     private boolean isDeleted;
     private String type;
     private String etc;

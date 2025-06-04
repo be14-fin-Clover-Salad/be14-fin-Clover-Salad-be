e42d6c433fe132dc484004d4f333b5f491ac73a5
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-06-04 09:12:25 +0900

    refactor : 계약 상세 조회 DTO 수정
    기존 테이블 별 DTO 관리 대신 Flat하게 변경
    하나의 DTO와 ResultMap 하나로 관리
    
    #70 #152

--- src/main/java/com/clover/salad/contract/query/dto/ContractResponseDTO.java
+++ src/main/java/com/clover/salad/contract/query/dto/ContractResponseDTO.java
@@ -6,14 +6,37 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+
 @Getter
 @Setter
-@ToString
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ContractResponseDTO {
+
 	private int id;
 	private String renameFile;
 	private String code;
 	private int amount;
+
+
+	private String customerName;
+	private String customerAddress;
+	private String customerPhone;
+
+
+	private String employeeName;
+
+
+	private String aprvTitle;
+	private String aprvContent;
+	private String aprvState;
+	private int reqId;
+	private int aprvId;
+
+
+	private String productName;
+	private String serialNumber;
+	private int quantity;
+	private int rentalCost;
 }

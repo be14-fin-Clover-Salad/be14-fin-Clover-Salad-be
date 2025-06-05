7d3f5e6d4a83d6116cf152bdcd8d9884a62dfe1c
Bluesky3125 <charlie4822@gmail.com>
2025-06-05 17:39:10 +0900

    Style: DepartmentPerformanceDTO 들여쓰기 수정

--- src/main/java/com/clover/salad/performance/command/application/dto/DepartmentPerformanceDTO.java
+++ src/main/java/com/clover/salad/performance/command/application/dto/DepartmentPerformanceDTO.java
@@ -13,20 +13,20 @@ import lombok.ToString;
 @ToString
 public class DepartmentPerformanceDTO {
 	private Integer id;
-		
-		private Integer rentalProductCount;
-		
-		private Integer rentalRetentionCount;
-		private Integer totalRentalCount;
-		
-		private Integer newCustomerCount;
-		
-		private Long totalRentalAmount;
-		
-		/* 설명. 1.0 -> 10으로 내부적으로 처리하고 외부로 보일 때는 사이에 .찍어서 표시하기 */
-		private Integer customerFeedbackScore;
-		private Integer customerFeedbackCount;
-		
-		private int targetDate;
-		private int departmentId;
+	
+	private Integer rentalProductCount;
+	
+	private Integer rentalRetentionCount;
+	private Integer totalRentalCount;
+	
+	private Integer newCustomerCount;
+	
+	private Long totalRentalAmount;
+	
+	/* 설명. 1.0 -> 10으로 내부적으로 처리하고 외부로 보일 때는 사이에 .찍어서 표시하기 */
+	private Integer customerFeedbackScore;
+	private Integer customerFeedbackCount;
+	
+	private int targetDate;
+	private int departmentId;
 }

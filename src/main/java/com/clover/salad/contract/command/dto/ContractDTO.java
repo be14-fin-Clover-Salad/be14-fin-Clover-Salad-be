894286f2468af947da6629a61bf0f74712d5a9f1
Gombo2 <rhtjddus0502@gmail.com>
2025-06-09 04:37:16 +0900

    feature : 계약등록 Enum타입 적용
    - 결제 여부와 본인과의 관계 Enum타입 처리

--- src/main/java/com/clover/salad/contract/command/dto/ContractDTO.java
+++ src/main/java/com/clover/salad/contract/command/dto/ContractDTO.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com.clover.salad.contract.command.entity.ContractEntity;
+import com.clover.salad.contract.common.ContractStatus;
+import com.clover.salad.contract.common.RelationshipType;
 import com.clover.salad.contract.document.entity.DocumentOrigin;
 import com.clover.salad.customer.command.domain.aggregate.entity.Customer;
 import com.clover.salad.employee.command.domain.aggregate.entity.EmployeeEntity;
@@ -58,9 +60,9 @@ public class ContractDTO {
 			.bankAccount(bankAccount)
 			.paymentDay(paymentDay)
 			.depositOwner(depositOwner)
-			.relationship(relationship)
+			.relationship(RelationshipType.from(relationship))
 			.paymentEmail(paymentEmail)
-			.status("결재 전")
+			.status(ContractStatus.DEFAULT)
 			.isDeleted(false)
 			.etc(null)
 			.documentOrigin(documentOrigin)

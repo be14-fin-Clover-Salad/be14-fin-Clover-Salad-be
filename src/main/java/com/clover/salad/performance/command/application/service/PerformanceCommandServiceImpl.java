ff5d93ca5dfd8f03d68242959a3fb390b294f171
Bluesky3125 <charlie4822@gmail.com>
2025-06-09 12:07:33 +0900

    Feat: 사원 실적 계약 파트 추가 중간 저장용

--- src/main/java/com/clover/salad/performance/command/application/service/PerformanceCommandServiceImpl.java
+++ src/main/java/com/clover/salad/performance/command/application/service/PerformanceCommandServiceImpl.java
@@ -4,8 +4,11 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,8 @@ import com.clover.salad.common.exception.EmployeeNotFoundException;
 import com.clover.salad.contract.query.dto.ContractDTO;
 import com.clover.salad.contract.query.dto.ContractSearchDTO;
 import com.clover.salad.contract.query.service.ContractService;
+import com.clover.salad.customer.command.domain.repository.CustomerRepository;
+import com.clover.salad.customer.query.service.CustomerQueryService;
 import com.clover.salad.employee.query.dto.EmployeeQueryDTO;
 import com.clover.salad.employee.query.dto.SearchEmployeeDTO;
 import com.clover.salad.employee.query.service.EmployeeQueryService;
@@ -31,6 +36,8 @@ public class PerformanceCommandServiceImpl implements PerformanceCommandService 
 	private final EmployeePerformanceRepository employeePerformanceRepository;
 	private final EmployeeQueryService employeeQueryService;
 	private final ContractService contractService;
+	private final CustomerRepository customerRepository;
+	private final CustomerQueryService customerQueryService;
 	
 	@Override
 	public void refreshContractPerformance(String employeeCode) {
@@ -43,9 +50,10 @@ public class PerformanceCommandServiceImpl implements PerformanceCommandService 
 		
 		EmployeePerformance currentEP = employeePerformanceRepository.findByEmployeeIdAndTargetDate(employeeId, targetDate);
 		
+		/* 설명. 계약 조회를 위한 검색 조건 생성 */
 		ContractSearchDTO contractSearchDTO = new ContractSearchDTO();
 		contractSearchDTO.setEmployeeId(employeeId);
-		
+		/* 설명. 검색 조건 중 시간 설정 */
 		LocalDate nowLD = now.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 		LocalDate startDateStart = nowLD.with(TemporalAdjusters.firstDayOfMonth());
 		LocalDate startDateEnd = nowLD.with(TemporalAdjusters.lastDayOfMonth());
@@ -54,9 +62,41 @@ public class PerformanceCommandServiceImpl implements PerformanceCommandService 
 		
 		List<ContractDTO> contractDTOList = contractService.searchContracts(contractSearchDTO);
 		EmployeePerformanceDTO epDTO = new EmployeePerformanceDTO();
+		
+		int rentalProductCount = 0;
+		int totalRentalCount = contractDTOList.size();
+		/* 설명. totalRentalCount 이번 달의 총 계약 수 */
+		epDTO.setTotalRentalCount(totalRentalCount);
+		int rentalRetentionCount = totalRentalCount;
+		long totalRentalAmount = 0L;
+		Set<Integer> newCustomerIdSet = new HashSet<>();
+		
 		for (ContractDTO contractDTO : contractDTOList) {
-			contractDTO;
+			/* 설명. rentalProductCount 렌탈 상품 수 */
+			// List<ContractProductEntity> cpEntityList = contractProductRepository.findByContractId(contractDTO.getId());
+			// for (ContractProductEntity cpEntity : cpEntityList) {
+			// 	rentalProductCount += cpEntity.getQuantity();
+			// }
+			/* 설명. rentalRetentionCount 계약 유지 건수 = 이번 달 총 계약 수 - 이번 달에 시작된 계약 수 */
+			if (contractDTO.getStartDate().isAfter(startDateStart)
+			 || contractDTO.getStartDate().isEqual(startDateStart)) {
+				rentalRetentionCount--;
+			}
+			/* 설명. newCustomerCount 신규 고객 수: 리스트로 만들고 id 개수를 세는 로직 */
+			// int currentCustomerId = contractDTO.getCustomerId();
+			// LocalDate currentCustomerRegisterDate = LocalDate.parse(customerQueryService.findCustomerById(currentCustomerId).getRegisterAt());
+			// if (currentCustomerRegisterDate.isAfter(startDateStart)
+			//  || currentCustomerRegisterDate.isEqual(startDateStart)) {
+			// 	newCustomerIdSet.add(currentCustomerId);
+			// }
+			/* 설명. totalRentalAmount 총 렌탈 금액 */
+			totalRentalAmount += contractDTO.getAmount();
 		}
+		
+		epDTO.setRentalProductCount(rentalProductCount);
+		epDTO.setRentalRetentionCount(rentalRetentionCount);
+		epDTO.setNewCustomerCount(newCustomerIdSet.size());
+		epDTO.setTotalRentalAmount(totalRentalAmount);
 		
 		/* 설명. 조회 시 없으면 새로 만들기, 있으면 업데이트하기 */
 		if (currentEP == null) {

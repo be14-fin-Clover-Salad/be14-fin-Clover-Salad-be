1809a2742bd8ac75b05f9be7875b5c3191ccb2e9
jakego1 <jakego13129@gmail.com>
2025-06-11 16:00:11 +0900

    로그인한 사원이 자신의 권한에 맞는 결재의 상세 정보를 조회할 수 있는 기능 구현

---
+++ src/main/java/com/clover/salad/approval/query/dto/ApprovalDetailDTO.java
@@ -0,0 +1,29 @@
+package com.clover.salad.approval.query.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ApprovalDetailDTO {
+	private int id;
+	private String code;
+	private String title;
+	private String content;
+	private LocalDateTime reqDate;
+	private LocalDateTime aprvDate;
+	private String state; // Enum이 아닌 각 Enum의 한글 Label
+	private String comment;
+	private String reqName;
+	private String aprvName;
+	private String contractCode;
+
+	private Integer reqId;
+	private Integer aprvId;
+}
--- src/main/java/com/clover/salad/approval/query/mapper/ApprovalMapper.java
+++ src/main/java/com/clover/salad/approval/query/mapper/ApprovalMapper.java
@@ -3,13 +3,16 @@ package com.clover.salad.approval.query.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import com.clover.salad.approval.query.dto.ApprovalDetailDTO;
 import com.clover.salad.approval.query.dto.ApprovalSearchRequestDTO;
 import com.clover.salad.approval.query.dto.ApprovalSearchResponseDTO;
 
 @Mapper
 public interface ApprovalMapper {
 
+	/* 설명. 결재 내역 목록 조회 */
 	// 관리자
 	List<ApprovalSearchResponseDTO> searchApprovals(ApprovalSearchRequestDTO request);
 
@@ -18,4 +21,7 @@ public interface ApprovalMapper {
 
 	// 팀장
 	List<ApprovalSearchResponseDTO> searchByApprover(ApprovalSearchRequestDTO request);
+
+	/* 설명. 결재 내역 상세 조회 */
+	ApprovalDetailDTO findApprovalDetailById(@Param("id") int id);
 }

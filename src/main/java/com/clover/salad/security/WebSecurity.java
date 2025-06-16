99ece587c1958a5978fa306d066758da84603fc6
jakego1 <jakego13129@gmail.com>
2025-06-16 15:12:18 +0900

    알림에 SSE 기능 추가

--- src/main/java/com/clover/salad/security/WebSecurity.java
+++ src/main/java/com/clover/salad/security/WebSecurity.java
@@ -46,10 +46,11 @@ public class WebSecurity {
 		http.authorizeHttpRequests(authz ->
 				authz
 					// 개발용 일시적 허용
-					.requestMatchers("/**").permitAll()
+					// .requestMatchers("/**").permitAll()
 					.requestMatchers("/auth/login").permitAll()
 					.requestMatchers("/employee").permitAll()
 					.requestMatchers("/employee/**").permitAll()
+					.requestMatchers("/notification/subscribe").permitAll()
 					.anyRequest().authenticated()
 			)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));

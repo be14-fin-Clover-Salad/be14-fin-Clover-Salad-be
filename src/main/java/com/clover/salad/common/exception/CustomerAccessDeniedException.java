97b85aa057673d18a3ee2e1cb4ccb8486b6f243b
50071 <rkd50071@gmail.com>
2025-06-16 15:14:13 +0900

    Env: 고객 관련 예외 처리

--- src/main/java/com/clover/salad/common/exception/CustomerAccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package com.clover.salad.common.exception;
-
-public class CustomerAccessDeniedException extends RuntimeException {
-    public CustomerAccessDeniedException(String message) {
-        super(message);
-    }
-}
